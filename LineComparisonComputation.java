8c96a3172c0d8c696e56e602fcdf91d94ed00f1a
akshat1032 <akshat.30oct@gmail.com>
2020-09-25 21:02:21 +0530

    Refactor : Comparison Of Length Of Two Lines Using CompareTo And Equals Refactored

--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -1,48 +1,75 @@
 package com.capgemini.linecomparisoncomputation;
 
 public class LineComparisonComputation {
-
-	public static void main(String[] args) {
+	
+	//Constants
+	//Co-ordinates of Line 1
+	//Assuming the coordinates to be less than 100
+	public static final int LINE_ONE_X1 = (int)(Math.floor(Math.random() * 100));
+	public static final int LINE_ONE_Y1 = (int)(Math.floor(Math.random() * 100));
+	public static final int LINE_ONE_X2 = (int)(Math.floor(Math.random() * 100));
+	public static final int LINE_ONE_Y2 = (int)(Math.floor(Math.random() * 100));
+			
+	//Co-ordinates of Line 2
+	//Assuming the coordinates to be less than 100
+	public static final int LINE_TWO_X1 = (int)(Math.floor(Math.random() * 100));
+	public static final int LINE_TWO_Y1 = (int)(Math.floor(Math.random() * 100));
+	public static final int LINE_TWO_X2 = (int)(Math.floor(Math.random() * 100));
+	public static final int LINE_TWO_Y2 = (int)(Math.floor(Math.random() * 100));
+	
+	//Variable
+	private double lengthLineOne = 0;
+	private double lengthLineTwo = 0;
+	private String lineOneLength = "";
+	private String lineTwoLength = "";
+	private int compareToValue = 0;
+	
+	//Calculating length of line and storing in String
+	public void calculateLength() {
 		
-		//Variable
-		double lengthLineOne = 0;
-		double lengthLineTwo = 0;
-		String lineOneLength = "";
-		String lineTwoLength = "";
-		int compareToValue = 0;
+		this.lengthLineOne = Math.sqrt((Math.pow(LINE_ONE_X2-LINE_ONE_X1, 2)+Math.pow(LINE_ONE_Y2-LINE_ONE_Y1, 2)));
+		this.lengthLineTwo = Math.sqrt((Math.pow(LINE_TWO_X2-LINE_TWO_X1, 2)+Math.pow(LINE_TWO_Y2-LINE_TWO_Y1, 2)));
+		this.lineOneLength = String.valueOf(lengthLineOne);
+		this.lineTwoLength = String.valueOf(lengthLineTwo);
+	}
+	
+	//Comparing using equals
+	public boolean compareUsingEquals() {
+		return (this.lineOneLength.equals(this.lineTwoLength));
+	}
+	
+	//Comparing using compareTo
+	public int compareUsingCompareTo() {
 		
-		//Co-ordinates of Line 1
-		//Assuming the coordinates to be less than 100
-		int lineOneX1 = (int)(Math.floor(Math.random() * 100));
-		int lineOneY1 = (int)(Math.floor(Math.random() * 100));
-		int lineOneX2 = (int)(Math.floor(Math.random() * 100));
-		int lineOneY2 = (int)(Math.floor(Math.random() * 100));
-		
-		//Co-ordinates of Line 2
-		//Assuming the coordinates to be less than 100
-		int lineTwoX1 = (int)(Math.floor(Math.random() * 100));
-		int lineTwoY1 = (int)(Math.floor(Math.random() * 100));
-		int lineTwoX2 = (int)(Math.floor(Math.random() * 100));
-		int lineTwoY2 = (int)(Math.floor(Math.random() * 100));
-		
-		//Printing Co-ordinates of two lines
-		System.out.println("Co-ordinates of Line One (x1,y1) (x2,y2)  : "+"("+lineOneX1+","+lineOneY1+")"+" "+"("+lineOneX2+","+lineOneY2+")");
-		System.out.println("Co-ordinates of Line Two (x1,y1) (x2,y2)  : "+"("+lineTwoX1+","+lineTwoY1+")"+" "+"("+lineTwoX2+","+lineTwoY2+")");
-		
-		//Calculating length of the line and storing as String 
-		lengthLineOne = Math.sqrt((Math.pow(lineOneX2-lineOneX1, 2)+Math.pow(lineOneY2-lineOneY1, 2)));
-		lengthLineTwo =  Math.sqrt((Math.pow(lineTwoX2-lineTwoX1, 2)+Math.pow(lineTwoY2-lineTwoY1, 2)));
-		lineOneLength = String.valueOf(lengthLineOne);
-		lineTwoLength = String.valueOf(lengthLineTwo);
-		
-		//Comparing the length of two lines
-		compareToValue = lineOneLength.compareTo(lineTwoLength);
-		if (compareToValue == 0) {
+		this.compareToValue = this.lineOneLength.compareTo(this.lineTwoLength);
+		return this.compareToValue;
+	}
+	
+	//Printing
+	public void printComparisonMessage() {
+		boolean lengthCheckEquals = this.compareUsingEquals();
+		double lengthCheckCompareTo = this.compareUsingCompareTo();
+		if (lengthCheckEquals && lengthCheckCompareTo == 0) {
 			System.out.println("The two lines are equal in length");
-		} else if(compareToValue > 0) {
+		}
+		else if (lengthCheckCompareTo > 0) {
 			System.out.println("Line One is longer than Line Two");
-		} else {
+		}
+		else {
 			System.out.println("Line Two is longer than Line One");
 		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		LineComparisonComputation lengthObject = new LineComparisonComputation();
+		
+		lengthObject.calculateLength();
+
+		//Printing Co-ordinates of two lines
+		System.out.println("Co-ordinates of Line One (x1,y1) (x2,y2)  : "+"("+LINE_ONE_X1+","+LINE_ONE_Y1+")"+" "+"("+LINE_ONE_X2+","+LINE_ONE_Y2+")");
+		System.out.println("Co-ordinates of Line Two (x1,y1) (x2,y2)  : "+"("+LINE_TWO_X1+","+LINE_TWO_Y1+")"+" "+"("+LINE_TWO_X2+","+LINE_TWO_Y2+")");
+		lengthObject.printComparisonMessage();
 	}
 }
