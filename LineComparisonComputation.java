cf538464b1b856f83a5bb05511822ed38f197258
akshat1032 <akshat.30oct@gmail.com>
2020-09-25 19:02:47 +0530

    Added : Welcome Message Added

---
+++ LineComparisonComputation.java
@@ -0,0 +1,10 @@
+package com.capgemini.linecomparisoncomputation;
+
+public class LineComparisonComputation {
+
+	public static void main(String[] args) {
+		
+		//Welcome Message Display
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+}
